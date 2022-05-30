91e71a36216642e157d1654f6cc72321886e1e3d
Ridheman <76870043+Ridheman@users.noreply.github.com>
2022-05-30 12:54:27 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function setup() {
   rocket = createSprite(200, 200, 50, 50);
   rocket.addImage(rocketImg);
   rocket.scale = 0.5
-  rocket.debug = true;
+  rocket.debug = false;
 
   gameOver = createSprite(width/2,height/2);
   gameOver.addImage(gameOverImg);
@@ -87,7 +87,7 @@ function draw() {
 function spawnBullet(){
   bullet = createSprite(rocket.x,rocket.y,10,5);
   bullet.addImage(bulletImg);
-  bullet.debug = true;
+  bullet.debug = false;
   bullet.setCollider("rectangle",0,0,350,130);
   bullet.scale = 0.2;
   bullet.velocityX = 5 + (score/20);
@@ -99,7 +99,7 @@ function spawnAsteroid(){
   if(frameCount%60===0){
     asteroid = createSprite(width,height/2,20,100);
     asteroid.addImage(asteroidImg);
-    asteroid.debug = true;
+    asteroid.debug = false;
     asteroid.setCollider("rectangle",0,0,350,130);
     asteroid.scale = 0.5;
     asteroid.y=Math.round(random(100,height-100));
